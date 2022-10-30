9e84b755cf828b1fc558a42f46fd30ad8321bf99
sai-siril <g.saisiril@gmail.com>
2022-10-30 16:08:44 -0400

    Adding acceleratoin code

--- sourcecode.c
+++ sourcecode.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
-
+#include <conio.h>
 int main(void)
 {
-  printf("hello world");
+  printf("\n-------Accleration in Terminal-------\n");
+  printf("Hit w to speed up, Do not touch 180 mph\n");
+  printf("Hit x to Exit\n");
+  printf("-----------------------------------------\n");
+  char c = getch();
+  int i = 0;
+  if(c = 'w')
+  {
+    while(1)
+    {
+      
+      if('w' == getch())
+      {
+        printf("%d mph",i++);
+        printf("\b\b\b\b\b\b\b\b");
+        if(i==185)
+        {
+          printf("\n185 mph is a lot of speed, stop!\n");
+          break;
+        }
+      }
+      if('x' == getch())
+      {
+        break;
+      }
+    }  
+
+  }
+  printf("------------------------\n");
+  printf("\n exiting acceleration");
+  
   return 0;
 }
